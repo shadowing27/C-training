821df8c5311546881b9146c06850227f83e122d8
shadow <1260126174@qq.com>
2023-07-20 16:00:45 +0800

    0720

---
+++ test0720/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main() 
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d", a);
+//	return 0;
+//}
+int main()
+{
+	printf("hello bit");
+	return 0;
+}
