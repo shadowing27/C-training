3e1bab87b4e952df3887097491817b8e92456295
shadow <1260126174@qq.com>
2023-07-22 22:18:34 +0800

    刷题训练营第一讲

---
+++ 130-No.1/源.c
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//BC1 Hello Nowcoder
+//
+//#include<stdio.h>
+//int main()
+//{
+//    printf("Hello Nowcoder!");
+//    return 0;
+//}
+
+//BC2 小飞机
+//
+//#include <stdio.h>
+//int main()
+//{
+//    printf("     **\n");
+//    printf("     **\n");
+//    printf("************\n");
+//    printf("************\n");
+//    printf("    *  *\n");
+//    printf("    *  *\n");
+//    return 0;
+//}
+
+//原BC3 有容乃大
+//
+//#include <stdio.h>
+//int main()
+//{
+//	printf("The size of short is %d bytes.\n", sizeof(short));
+//	printf("The size of int is %d bytes.\n", sizeof(int));
+//	printf("The size of long is %d bytes.\n", sizeof(long));
+//	printf("The size of long long is %d bytes.\n", sizeof(long long));
+//	return 0;
+//}
+
+//BC17 缩短二进制
+//
+//#include <stdio.h>
+//int main()
+//{
+//    printf("%#o %#X", 1234, 1234);
+//    return 0;
+//}
+
+//BC94 反向输出一个四位数
+//
+//#include <stdio.h>
+//int main()
+//{
+//    int n = 0, i = 0;
+//    scanf("%d", &n);
+//    for (i = 0; i < 4; i++)
+//    {
+//        printf("%d", n % 10);
+//        n /= 10;
+//    }
+//    return 0;
+//}
+
+//BC15 大小写转换
+//
+//#include <stdio.h>
+//int main()
+//{
+//    int ch;
+//    while ((ch = getchar()) != EOF)
+//    {
+//        printf("%c\n", ch += 32);
+//        getchar();
+//    }
+//    return 0;
+//}
