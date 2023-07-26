cd0986dcae3864a11fb8569d4b1fa2d3f6832811
shadow <1260126174@qq.com>
2023-07-26 23:57:45 +0800

    0726修改

--- homework0724/homework0724.c
+++ homework0724/homework0724.c
@@ -101,6 +101,24 @@
 
 //写一个代码：打印100~200之间的素数
 
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	int i = 0, j = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		for (j = 2; j <=sqrt(i); j++)
+//		{
+//			if (i % j == 0)
+//				break;
+//		}
+//		if(j>sqrt(i))
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
 #include <stdio.h>
 #include <math.h>
 int main()
@@ -108,12 +126,12 @@ int main()
 	int i = 0, j = 0;
 	for (i = 100; i <= 200; i++)
 	{
-		for (j = 2; j <=sqrt(i); j++)
+		for (j = 2; j <= i-1 ; j++)
 		{
 			if (i % j == 0)
 				break;
 		}
-		if(j>sqrt(i))
+		if (j > sqrt(i))
 			printf("%d ", i);
 	}
 	return 0;
