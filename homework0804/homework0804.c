4f67adb61ad4c7fff7494ae2bb10f556f5f78e61
shadow <1260126174@qq.com>
2023-09-20 17:51:58 +0800

    0804作业

--- homework0804/homework0804.c
+++ homework0804/homework0804.c
@@ -72,3 +72,82 @@
 //	printf("%d的%d次方为%d", n, k, m(n, k));
 //	return 0;
 //}
+
+//计算一个数的每位之和（递归实现）
+//写一个递归函数DigitSum(n)，输入一个非负整数，返回组成它的数字之和
+//例如，调用DigitSum(1729)，则应该返回1 + 7 + 2 + 9，它的和是19
+//输入：1729，输出：19
+
+//#include <stdio.h>
+//int DigitSum(n)
+//{
+//	static int sum = 0;
+//	sum += n % 10;
+//	if (n / 10 != 0)
+//		DigitSum(n / 10);
+//	return sum;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", DigitSum(n));
+//	return 0;
+//}
+
+//递归和非递归分别实现求n的阶乘（不考虑溢出的问题）
+
+//非递归
+//#include <stdio.h>
+//int multiple(int n)
+//{
+//	int x = 1, i = 0;
+//	for (i = 1; i <= n; i++)
+//		x *= i;
+//	return x;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", multiple(n));
+//	return 0;
+//}
+//非递归
+//#include <stdio.h>
+//int multiple(int n)
+//{
+//	static int x = 1;
+//	if (n - 1 > 0)
+//	{
+//		x = n * multiple(n - 1);
+//		return x;
+//	}
+//	return x;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", multiple(n));
+//	return 0;
+//}
+
+//递归方式实现打印一个整数的每一位
+
+#include <stdio.h>
+void print(int n)
+{
+	int i = n % 10;
+	n = n / 10;
+	if (n > 0)
+		print(n);
+	printf("%d ", i);
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	print(n);
+	return 0;
+}
