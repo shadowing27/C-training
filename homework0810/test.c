d648604b03899e4faac03e550a74c19cf485d2e8
shadow <1260126174@qq.com>
2023-10-05 20:00:21 +0800

    0810作业

--- homework0810/test.c
+++ homework0810/test.c
@@ -58,39 +58,101 @@
 //来调整该数组中数字的顺序使得数组中所有的奇数位于数组的前半部分，
 //所有偶数位于数组的后半部分。
 
+//#include <stdio.h>
+//void func(int* arr)
+//{
+//	int i = 0, j = 0;
+//	int a = 0, b = 0;
+//	int temp1[10] = { 0 };
+//	int temp2[10] = { 0 };
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (arr[i] % 2 == 1)
+//		{
+//			temp1[a] = arr[i];
+//			a++;
+//		}
+//		else
+//		{
+//			temp2[b] = arr[i];
+//			b++;
+//		}
+//	}
+//	for(i=0;i<10;i++)
+//	{
+//		if (i < a)
+//			arr[i] = temp1[i];
+//		else
+//			arr[i] = temp2[i-a];
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(arr + i));
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", arr + i);
+//	}
+//	func(arr);
+//	return 0;
+//}
+
+//写一个函数，判断一个字符串是否为另外一个字符串旋转之后的字符串。
+
+//例如：给定s1 = AABCD和s2 = BCDAA，返回1
+//给定s1 = abcd和s2 = ACBD，返回0.
+
+//AABCD左旋一个字符得到ABCDA
+//AABCD左旋两个字符得到BCDAA
+//AABCD右旋一个字符得到DAABC
+
+#include <string.h>
 #include <stdio.h>
-void func(int* arr)
+int judge(char* str1,char* str2)
 {
-	int i = 0, j = 0;
-	int odd = 0;
-	for (i = 0; i < 10; i++)
+	//判断两个字符串长度是否相同
+	int len1 = strlen(str1), len2 = strlen(str2);
+	if(len1!=len2)
+		return 0;
+	else
 	{
-		if (arr[i] % 2 == 1)
-			odd++;
+		//str1逐字符与str比较 ^=0时比较下一字符
+		int i = 0, j = 0;
+		for (i = 0; i < len1; i++)
+		{
+			if ((str1[i] ^ str2[j]) == 0)
+				j++;
+			if (j >= len2)
+				break;
+		}
+		int temp = j;
+		if(temp<len2)
+		{
+			for (i = 0; i < len1-1-temp; i++)
+			{
+				if ((str1[i] ^ str2[j]) == 0)
+					j++;
+				if (j >= len2)
+					break;
+			}
+		}
+		if (j < len2 - 1)
+			return 0;
+		else
+			return 1;
 	}
-	for (i = 0; i < odd; i++)
-	{
-		if (arr[i] % 2 == 1)
-			continue;
-
-	}
-
-
-
-
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", *(arr + i));
-	}
+	
 }
 int main()
 {
-	int arr[10] = { 0 };
-	int i = 0;
-	for (i = 0; i < 10; i++)
-	{
-		scanf("%d", arr + i);
-	}
-	func(arr);
+	char str1[10] = { 0 };
+	char str2[10] = { 0 };
+	scanf("%s %s", str1, str2);
+	printf("%d\n", judge(str1, str2));
 	return 0;
 }
